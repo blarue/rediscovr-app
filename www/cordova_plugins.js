373b6a2fe076741ce8eec287ecb4278bdb13adcd
Ben LaRue <blarue@gmail.com>
2014-02-11 22:54:31 -0500

    this change doesn't seem to do too much

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -1,24 +1,77 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
-        "id": "org.apache.cordova.console.console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/logger.js",
-        "id": "org.apache.cordova.console.logger",
-        "clobbers": [
-            "cordova.logger"
-        ]
-    }
-];
-module.exports.metadata = 
+    module.exports = [
+        {
+            "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+            "id": "org.apache.cordova.console.console",
+            "clobbers": [
+                "console"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.console/www/logger.js",
+            "id": "org.apache.cordova.console.logger",
+            "clobbers": [
+                "cordova.logger"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.network-information/www/network.js",
+            "id": "org.apache.cordova.network-information.network",
+            "clobbers": [
+                "navigator.connection",
+                "navigator.network.connection"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+            "id": "org.apache.cordova.network-information.Connection",
+            "clobbers": [
+                "Connection"
+            ]
+        },
+        {
+            "file": "plugins/nl.x-services.plugins.calendar/www/Calendar.js",
+            "id": "nl.x-services.plugins.calendar.Calendar",
+            "clobbers": [
+                "Calendar"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+            "id": "org.apache.cordova.geolocation.Coordinates",
+            "clobbers": [
+                "Coordinates"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+            "id": "org.apache.cordova.geolocation.PositionError",
+            "clobbers": [
+                "PositionError"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+            "id": "org.apache.cordova.geolocation.Position",
+            "clobbers": [
+                "Position"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+            "id": "org.apache.cordova.geolocation.geolocation",
+            "clobbers": [
+                "navigator.geolocation"
+            ]
+        }
+    ];
+    module.exports.metadata =
 // TOP OF METADATA
-{
-    "org.apache.cordova.console": "0.2.6"
-}
+    {
+        "org.apache.cordova.console": "0.2.6",
+        "org.apache.cordova.network-information": "0.2.7",
+        "nl.x-services.plugins.calendar": "3.2",
+        "org.apache.cordova.geolocation": "0.3.6"
+    }
 // BOTTOM OF METADATA
 });
